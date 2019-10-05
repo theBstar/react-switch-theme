d0cab28026dbc862ab1e6df3feba7cc81319cd1d
Bikram <6bikramsutar@gmail.com>
2019-10-06 00:16:08 +0530

    initial version tested and verified

--- src/index.js
+++ src/index.js
@@ -1,55 +1,56 @@
-import React from 'react';
+import { useState, useEffect } from 'react';
 
 /* eslint-disable no-console */
+
+function addOfflineThemeWithCB(offlineStorageKey, callback) {
+  if (!offlineStorageKey) {
+    let warnMessage = '[react-switch-theme]: offlineStorageKey should be ';
+    warnMessage += 'provided to support offline theme storage';
+    console.debug(warnMessage);
+    return;
+  }
+  if (localStorage) {
+    callback(offlineStorageKey);
+  }
+}
 
 function useReactSwitchTheme({
   colors,
   activeMode,
-  offlineSupport,
   offlineStorageKey,
-} = { offlineSupport: true }) {
-  if (colors.indexOf(activeMode) === -1) {
+}) {
+  // eslint-disable-next-line no-restricted-syntax
+  const colorsKeys = Object.keys(colors);
+
+  if (colorsKeys.indexOf(activeMode) === -1) {
     const errorMessage = '[react-switch-theme]: activeMode should be one of the key of colors';
     console.error(errorMessage);
     throw Error(errorMessage);
   }
 
-  function addOfflineThemeWithCB(callback) {
-    if (!offlineSupport) return;
-
-    if (!offlineStorageKey) {
-      const errorMessage = '[react-switch-theme]: offlineStorgeKey should be provided';
-      console.error(errorMessage);
-      throw Error(errorMessage);
-    }
+  let actualActiveMode = activeMode;
+  if (offlineStorageKey) {
     if (localStorage) {
-      callback();
-    } else {
-      console.warn('[react-switch-theme]: Could not support offline theming');
+      const previousMode = localStorage.getItem(offlineStorageKey);
+      if (previousMode) {
+        actualActiveMode = previousMode;
+      }
     }
   }
-
-  const [currentMode, setCurrentMode] = React.useState(activeMode);
-  const colorsKeys = Object.keys(colors);
-
-  addOfflineThemeWithCB(() => {
-    const previousMode = localStorage.getItem(offlineStorageKey);
-    setCurrentMode(previousMode);
-  });
-
+  const [currentMode, setCurrentMode] = useState(actualActiveMode);
 
   function setDiffMode() {
-    const currentModeIndex = colors.indexOf(currentMode);
+    const currentModeIndex = colorsKeys.indexOf(currentMode);
     const nextModeIndex = (currentModeIndex === 0 ? 1 : 0);
     const nextColorMode = colorsKeys[nextModeIndex];
 
-    addOfflineThemeWithCB(() => {
-      localStorage.setItem(offlineStorageKey, nextColorMode);
-      setCurrentMode(nextColorMode);
+    addOfflineThemeWithCB(offlineStorageKey, (key) => {
+      localStorage.setItem(key, nextColorMode);
     });
+    setCurrentMode(nextColorMode);
   }
 
-  React.useEffect(() => {
+  useEffect(() => {
     const nextColors = colors[currentMode];
 
     // eslint-disable-next-line no-restricted-syntax
--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,8 @@
 const path = require('path');
 
 module.exports = {
-  entry: '.src/index.js',
+  mode: 'production',
+  entry: './src/index.js',
   output: {
     path: path.resolve(__dirname, 'build'),
     filename: 'index.js',
